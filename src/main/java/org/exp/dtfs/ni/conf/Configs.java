7bc0a07f221a5b5021c642560babec4d644e2d38
陈金曈 <619011742@qq.com>
2018-08-24 23:14:49 +0800

    Deleted useless code comments.

--- src/main/java/org/exp/dtfs/ni/conf/Configs.java
+++ src/main/java/org/exp/dtfs/ni/conf/Configs.java
@@ -12,20 +12,11 @@ public class Configs {
     private static final String AMBARI_CONF_FILENAME = "dtfs-ni-ambari-site.xml";
     private static final String KAFKA_CONF_FILENAME = "dtfs-ni-kafka-site.xml";
     private static final String REST_CONF_FILENAME = "dtfs-ni-rest-site.xml";
-    // private static final String HADOOP_CONF_FILENAME =
-    // "dtfs-ni-hadoop-site.xml";
+    private static final String HADOOP_CONF_FILENAME = "dtfs-ni-hadoop-site.xml";
+    private static final String ZOOKEEPER_CONF_FILENAME = "dtfs-ni-zookeeper-site.xml";
 
-    // private static final String HADOOP_CORE_CONF_FILENAME = "core-site.xml";
     private static final String HDFS_CONF_FILENAME = "hdfs-site.xml";
     private static final String HBASE_CONF_FILENAME = "hbase-site.xml";
-
-    /*
-     * Hadoop configurations.
-     */
-    // private static final String HADOOP_CONF_MAIN_PATH_PARENT_KEY =
-    // "dtfs.ni.hadoop.conf.main.path.parent";
-    // private static final String HBASE_CONF_PATH_PARENT_KEY =
-    // "dtfs.ni.hbase.conf.main.path.parent";
 
     static {
         CONF.addResource(COMMON_CONF_FILENAME);
@@ -34,6 +25,8 @@ public class Configs {
         CONF.addResource(REST_CONF_FILENAME);
         CONF.addResource(HDFS_CONF_FILENAME);
         CONF.addResource(HBASE_CONF_FILENAME);
+        CONF.addResource(ZOOKEEPER_CONF_FILENAME);
+        CONF.addResource(HADOOP_CONF_FILENAME);
     }
 
     private Configs() {
@@ -43,19 +36,6 @@ public class Configs {
     private static Configuration getConf() {
         return CONF;
     }
-
-    // private static void addResources(String key, String... fileNames) {
-    // String path = get(key);
-    // LOG.info("Resource path is [" + path + "].");
-    // if (null == path || null == fileNames || 0 >= fileNames.length) {
-    // return;
-    // }
-    // for (String fileName : fileNames) {
-    // LOG.info("Resource file name is [" + fileName + "].");
-    // CONF.addResource(path + (path.endsWith(Constants.SLASH_DELIMITER) ? "" :
-    // Constants.SLASH_DELIMITER) + fileName);
-    // }
-    // }
 
     public static String get(String key) {
         return CONF.getTrimmed(key);
@@ -71,7 +51,7 @@ public class Configs {
 
     /**
      * For class HBaseUtils.
-     * 
+     *
      * @return
      */
     public static String getHBaseConfFileName() {
